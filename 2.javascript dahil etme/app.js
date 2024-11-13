6419939bedbe00e157036f133fcf52793b8b2ad9
dilaratasezenn <dilaratasezenn@gmail.com>
2024-11-14 02:38:38 +0300

    javascript dahil etme

---
+++ 2.javascript dahil etme/app.js
@@ -0,0 +1,5 @@
+console.log("dilara taşezen");
+/* biz burada consola yazı yazdık ama çalıştırdığımızda sayfada gözükmez çünkü indexte  de app.js ile 
+javascript yazdığımızı yazdığımızı belirtmedik bu yüzden index.html de <script src="app.js"></script> 
+yazdığımız zaman app.js de yazıdğımız kodlar index.html de yazıcağımız bu kod içerisinde yazmışız gibi olur
+ ve console.log da yazdığımız gözkür */
