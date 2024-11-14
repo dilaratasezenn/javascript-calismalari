1d281e4a4e1854f21f06583aff2762a5ef837fc8
dilaratasezenn <dilaratasezenn@gmail.com>
2024-11-14 20:27:56 +0300

    çıktı verme
    document, alert ve console ile kısaca nasıl çıktı verildiğine bakıldı

---
+++ 3.çıktı verme/app.js
@@ -0,0 +1,41 @@
+//document write ile çıktı verme
+document.write("Merhaba Dünya <br>"); // <br> ile alt satıra geçme çıktıda br tagı görünmez
+document.write("dilara taşezen <br>");
+document.write(5.5);//sayısal değerlerde tırnak kullanılmaz
+
+
+
+
+
+
+//console.log ile çıktı verme
+console.log("Merhaba Dünya");
+//sayfada gözükmez sayfada görmek için f12 console kısmına bakmak gerekli buraya herşey yazılabılır
+//console.clear();//console kısmındaki yazıları temizler
+
+//let a=5;
+//let b=3;
+//console.log("iki sayının toplamı:",a+b);//console da toplama işlemi de yapılabilir
+//console.log("iki sayının farkı:"+(a-b));
+//console da sayısal ve metinsel işlemler yapılabilir üstte virgül ile ayırdık altta artı işareti ve parantez ile ayırdık ikisi aynı şeyi yapar göstetrim farkı var
+
+
+
+
+
+console.log(window);//window objesi site içindeki tüm objeleri gösterir üst kısmıdaki sekme  dahil tüm objeleri gösterir
+//document objeside sayfa içindeki tüm objeleri gösterir
+
+console.log(document.location.host);//sayfanın hostunu gösterir
+
+console.clear();
+
+//alert (uyarı) popup ile çıktı verme
+//alert("Merhaba Dünya");
+//alert ile popup uyarı verir mesaj popup yani sayfada mesaj kutusu şeklinde gözükür
+
+//let a=20;
+//let b=10;
+//alert("iki sayının toplamı:"+(a+b));//matematiksel işlem yapılabilir
+
+alert("F5 tuşuna basmayınız");//şeklinde popup ile uyarı verilebilir kısaca alert bilgilendirmek ve uyarı için kullanılablir
